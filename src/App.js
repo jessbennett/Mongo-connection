cd75f1a413d9a97de7d545d621b7f261081d965d
Jess <homepc@Jesss-iMac.local>
2019-12-15 22:26:54 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -9,23 +9,15 @@ class App extends Component {
     data: null
   };
 
-  componentDidMount() {
-    // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callBackendAPI = async () => {
+  async componentDidMount() {
     const response = await fetch('/express_backend');
     const body = await response.json();
 
     if (response.status !== 200) {
       throw Error(body.message);
     }
-    return body;
-  };
+    console.log({ body });
+  }
 
   render() {
     return (
